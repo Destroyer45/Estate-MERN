3ac30a4d5e0b57ede7694150d797fb54c466020a
Destroyer45 <vickyeie@gmail.com>
2024-12-06 12:15:58 +0530

    create an app

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
+import Home from './pages/Home'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
-      <Route path='/' element={<Home/>} />
+      <Route path='/' element={<Home />} />
       <Route path='/About' element={<About/>} />
       <Route path='/Sign-in' element={<SignIn/>} />
       <Route path='/Sign-up' element={<SignUp/>} />
       <Route path='/Profile' element={<Profile/>} />
-
     </Routes>
     </BrowserRouter>
   )
